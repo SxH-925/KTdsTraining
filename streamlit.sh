73829bf90c4dfbb826656b1fb4138e887bcca9e7
SxH-925 <kimrubyredmine@kakao.com>
2025-06-17 14:46:43 +0900

    MVP 과제 ver0.2

--- streamlit.sh
+++ streamlit.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
+pip install python-dotenv
 pip install streamlit
 pip install streamlit-ace
+pip install OpenAI
 
 python -m streamlit run demo-app.py --server.port 8000 --server.address 0.0.0.0
